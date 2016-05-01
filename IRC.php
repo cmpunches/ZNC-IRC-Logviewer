12638179e3b698b011c28ecb2d3ddd595388135a
phanes <phanes@surroindustries.com>
2016-05-01 17:27:36 -0400

    index test

--- IRC.php
+++ IRC.php
@@ -2,15 +2,12 @@
 # Displays index with pulldown menu items for user, network, and channel.
 print('
 <html>
-<head>
-</head>
-<body>
-this is the body.
-</body>
+	<head>
+	</head>
+	<body>
+		this is the body.
+	</body>
 </html>
-
-
-
 ');
 
 
