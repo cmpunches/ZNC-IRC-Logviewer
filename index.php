db907a5b64cd268d781a1ce6b0b0ec285e86da9d
phanes <phanes@surroindustries.com>
2016-05-01 18:18:38 -0400

    templates with smarty

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+# Displays index with pulldown menu items for user, network, and channel.
+
+require_once('inc/smarty-3.1.29/libs/Smarty.class.php');
+
+$smarty = new Smarty();
+$smarty->template_dir = 'views';
+
+$smarty->compile_dir = 'tmp';
+
+$smarty->display('index.tpl');
+
+
+?>
